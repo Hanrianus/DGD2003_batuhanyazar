810378969738c76ec3152eadb9c529ed8452b3c9
Hanrianus <batuahnyazar56@gmail.com>
2026-04-19 16:56:09 +0300

    update

--- Assets/Script/Terminal Controller.cs
+++ Assets/Script/Terminal Controller.cs
@@ -1,47 +1,57 @@
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Events; 
 
 public class TerminalController : MonoBehaviour
 {
     [Header("Görsel Ayarlar")]
-    public CinemachineVirtualCamera terminalCamera; 
-    public GameObject hackGameUI; 
+    public CinemachineVirtualCamera terminalCamera;
+    public GameObject hackGameUI;
 
-    [Header("Kapý Ayarý")]
-    public DoorController targetDoor; 
+    [Header("VFX Ayarlarý")]
+    public GameObject successVFXPrefab;
+    public Transform vfxSpawnPoint;
+
+    [Header("Script Haberleþmesi (Unity Event)")]
+   
+    public UnityEvent onHackSuccessEvent;
 
     
     public void ActivateTerminal(GameObject player)
     {
-        
         if (hackGameUI != null) hackGameUI.SetActive(true);
 
-        
         if (terminalCamera != null)
         {
             terminalCamera.gameObject.SetActive(true);
-            terminalCamera.Priority = 20;
+            terminalCamera.Priority = 100;
         }
 
-        
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        
         PlayerMovement pm = player.GetComponent<PlayerMovement>();
-        if (pm != null)
-        {
-            pm.isHacking = true;
-        }
+        if (pm != null) pm.isHacking = true;
     }
 
-    
+   
     public void OnHackSuccess(GameObject player)
     {
         
-        if (hackGameUI != null) hackGameUI.SetActive(false);
+        if (successVFXPrefab != null)
+        {
+            Vector3 spawnPos = vfxSpawnPoint != null ? vfxSpawnPoint.position : transform.position;
+            GameObject effect = Instantiate(successVFXPrefab, spawnPos, Quaternion.identity);
+
+            ParticleSystem ps = effect.GetComponent<ParticleSystem>();
+            if (ps != null) ps.Play();
+
+            Destroy(effect, 4f);
+        }
 
         
+        if (hackGameUI != null) hackGameUI.SetActive(false);
+
         if (terminalCamera != null)
         {
             terminalCamera.Priority = 0;
@@ -50,21 +60,17 @@ public class TerminalController : MonoBehaviour
 
        
         PlayerMovement pm = player.GetComponent<PlayerMovement>();
-        if (pm != null)
-        {
-            pm.isHacking = false;
-        }
+        if (pm != null) pm.isHacking = false;
 
-        
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        
-        if (targetDoor != null)
+       
+        if (onHackSuccessEvent != null)
         {
-            targetDoor.OpenDoor();
+            onHackSuccessEvent.Invoke();
         }
 
-        Debug.Log("Hack Baþarýlý! Kapý tetiklendi.");
+        Debug.Log("Hack Baþarýlý! Unity Event tetiklendi.");
     }
 }
